eae41b9865b1a0ff0509fccf6bb94c2e1dd4b57d
zakaria <za.khiraoui@gmail.com>
2023-07-22 13:26:51 -0700

    printf group

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-
-int main() {
-    _printf("Character: %c\n", 'A');
-    _printf("String: %s\n", "I am a string !");
-    _printf("Percentage: %%\n");
-    return 0;
-}
