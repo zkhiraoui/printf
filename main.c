794f254ed8ac728a11d615bc228ece8bd4a1d917
zakaria <za.khiraoui@gmail.com>
2023-07-23 04:39:23 -0700

    printf group

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
-#include "main.h"
+#include <limits.h>
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    _printf("Let's try to printf a simple sentence.\n");
-    _printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    _printf("Percent:[%%]\n");
+    int len;
+    int len2;
 
-    /* Comparison with real printf */
-    printf("Let's try to printf a simple sentence.\n");
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    printf("Percent:[%%]\n");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    
+    // The following line has been commented out because '%r' is not a standard specifier
+    // _printf("Unknown:[%r]\n");
+    // printf("Unknown:[%r]\n");
+
     return (0);
 }
